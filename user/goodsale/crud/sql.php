37dcfb21ab1e2ab3d37a92520027cc3bda9b6613
anuwatjan <105624114+anuwatjan@users.noreply.github.com>
2022-06-23 09:58:23 +0700

    5
    5

--- user/goodsale/crud/sql.php
+++ user/goodsale/crud/sql.php
@@ -1,4 +1,5 @@
 <?php
+$datenow = date('Y-m-d');
 $year = date("Y");
 $date = date("Y-m-d");
 $week = date("N", strtotime($date));
@@ -11,13 +12,19 @@ $end = $date->format("Y-m-d");
 ?>
 
 <?php
-$datenow = date('Y-m-d');
-$sql = "SELECT * FROM store WHERE store_date = '$datenow'";
+
+$sql = "SELECT * , COUNT(a.product_id) AS count_product , SUM(a.store_total) AS count_price  FROM store a JOIN po b ON a.product_id = b.product_id WHERE a.store_date = '$datenow' group by a.store_date ";
 $query = mysqli_query($connection , $sql);
-print_r($query);
-exit;
+
+$sql1 = "SELECT * , COUNT(a.product_id) AS count_product , SUM(a.store_total) AS count_price  FROM store a JOIN po b ON a.product_id = b.product_id WHERE (a.store_date BETWEEN '$start' AND  '$end') group by a.store_date ";
+$query1 = mysqli_query($connection , $sql1);
+
+// print_r($query1);
+// exit;
+// print_r($query);
+// exit;
 // $result = mysqli_fetch_assoc($query);
-// while($row = mysqli_num_rows($query) > 1){
+// while($row = mysqli_num_rows(    $query) > 1){
 //     if()
 // }
 ?>
--- user/goodsale/index.php
+++ user/goodsale/index.php
@@ -19,28 +19,52 @@
   <div class="card info-card sales-card bg-primary">
     <div class="card-body text-white">
       <h2 class="card-title text-white" style="font-size: 30px;">ยอดขายสินค้าวันนี้<span></h2>
-
-      <div class="d-flex" style="font-size: 20px;">
-        <div class="ps-3">
-          <p><?= datethai($datenow) ?></p>
-          <?php 
-          while($row = mysqli_fetch_assoc($query)){ ?>
-          <p><?= $row['product_id']?></p>
+      <div class="row">
+        <div class="col">
+          <div class="d-flex" style="font-size: 20px;">
+            <div class="ps-3">
+              <p><?= datethai($datenow) ?></p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <?php
+          while ($row = mysqli_fetch_assoc($query)) { ?>
+            <p><?= $row['count_product'] ?> รายการ</p>
+            <p><?= number_format($row['count_price'], 2) ?> บาท</p>
           <?php } ?>
         </div>
+
       </div>
     </div>
   </div>
+</div>
+
+<div class="col-xxl-4 col-md-5">
   <div class="card info-card sales-card bg-success">
     <div class="card-body text-white">
       <h2 class="card-title text-white" style="font-size: 30px;">ยอดขายสินค้าสัปดาห์นี้</h2>
-<p>สัปดาห์ที่ <?= $week1 ?> </p>
-      <div class="d-flex" style="font-size: 20px;">
-        <div class="ps-3">
-          <p><?= datethai($start) ." - ". datethai($end) ?></p>
-
+      <p>สัปดาห์ที่ <?= $week1 ?> </p>
+      <div class="row">
+        <div class="col">
+          <div class="d-flex" style="font-size: 20px;">
+            <div class="ps-3">
+              <p><?= datethai($start) . " - " . datethai($end) ?></p>
+            </div>
+          </div>
         </div>
       </div>
+
+      <div class="col">
+      <?php
+          while ($row1 = mysqli_fetch_assoc($query1)) { ?>
+          <p><?=$row1['count_product'] ?></p>
+          <p><?= number_format($row1['count_price'], 2) ?> บาท</p>
+          <?php } ?>
+      </div>
+
+
     </div>
   </div>
+</div>
 </div><!-- End Reports -->
